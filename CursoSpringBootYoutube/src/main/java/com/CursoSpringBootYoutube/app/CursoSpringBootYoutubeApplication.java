9cf3a8521f80339283f9ceddfa96a48af01f7ae7
hugo2297 <hugodesusej@gmail.com>
2022-09-22 00:02:14 -0500

    Prueba para compartir repositorio

---
+++ CursoSpringBootYoutube/src/main/java/com/CursoSpringBootYoutube/app/CursoSpringBootYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.CursoSpringBootYoutube.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootYoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootYoutubeApplication.class, args);
+	}
+
+}
---
+++ CursoSpringBootYoutube/src/test/java/com/CursoSpringBootYoutube/app/CursoSpringBootYoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CursoSpringBootYoutube.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootYoutubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
